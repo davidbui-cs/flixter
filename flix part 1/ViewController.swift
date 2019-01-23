8f12298e802d0f4bc2eb6f5a441cfdd051a26b4a
David Bui <davidbui@davids-macbook-pro.local>
2019-01-23 00:10:50 -0800

    Initial Commit

---
+++ flix part 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix part 1
+//
+//  Created by David Bui on 1/23/19.
+//  Copyright © 2019 David Bui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
